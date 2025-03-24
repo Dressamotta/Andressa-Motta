4f6a2d9fdcac6591f01a89176b7ee14ed28cfb46
Dressamotta <Mottas.andressa@gmail.com>
2025-03-24 20:59:43 -0300

    Update aula11_exe_24032025.js

--- aula11_exe_24032025.js
+++ aula11_exe_24032025.js
@@ -1,1 +1,4 @@
-
+// Sistema de pedidos online
+// Modele classes para Produto (nome, preço, estoque),
+//Cliente (nome, endereço, carrinho) e Pedido (itens, total, status).
+//Implemente métodos para adicionar ao carrinho, finalizar compra e calcular frete.
