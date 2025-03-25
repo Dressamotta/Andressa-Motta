575fd8257fa6809d22a668042b78b84db51dec27
Dressamotta <Mottas.andressa@gmail.com>
2025-03-24 21:33:32 -0300

    Update aula11_exe_24032025.js

--- aula11_exe_24032025.js
+++ aula11_exe_24032025.js
@@ -2,3 +2,34 @@
 // Modele classes para Produto (nome, preço, estoque),
 //Cliente (nome, endereço, carrinho) e Pedido (itens, total, status).
 //Implemente métodos para adicionar ao carrinho, finalizar compra e calcular frete.
+
+
+class produto {
+    constructor(nome, preço, estoque) {
+        this.nome = nome;              
+        this.preço = preço;          
+        this.estoque = estoque;      
+
+    }
+
+}
+        class cliente {
+    constructor(nome,endereço,carrinho) {
+        this.nome = nome;
+        this.preço = nome;
+        this.carrinho= [];
+        
+
+    }
+
+}
+        
+        class Pedido {
+        constructor(itens,total,status) {
+            this.itens = itens;
+            this.total = total;
+            this.status = status;
+        }
+
+    }        
+
